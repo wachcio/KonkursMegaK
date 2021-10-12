8d9c7c566d38f4c42add65f060e628b38668838b
wachcio <wachcio@poczta.fm>
2021-10-13 01:19:26 +0200

    add todo working

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,6 @@
 (async () => {
   const addTaskBtn = document.querySelector('#addTaskBtn');
   const tasksList = document.querySelector('#tasksList');
-  const taskItemDone = document.querySelector('.taskItemDone');
-  const editTaskBtn = document.querySelector('.editTaskBtn');
-  const deleteTaskBtn = document.querySelector('.deleteTaskBtn');
   const addTaskDoneCheckbox = document.querySelector('#addTaskDoneCheckbox');
   const addTaskNameInput = document.querySelector('#addTaskNameInput');
 
@@ -26,13 +23,16 @@
   console.log({ tasks });
 
   const renderTodoList = async () => {
+    console.log(typeof tasks);
+
     if (tasks.length === 0) return;
 
     let HTML = '';
+    tasksList.innerHTML = '';
 
     tasks.map(({ name, done, id }, idx) => {
       HTML += `
-      <tr class="tasksItem bg-blue-300 lg:text-black">
+      <tr class="tasksItem bg-blue-300 lg:text-black" data-task-id="${id}">
                 <td class="taskItemOrdinalNumber p-3 font-medium text-gray-700">${idx + 1}.</td>
                 <td class="taskItemName p-3 font-medium text-gray-700">
                  ${name}
@@ -46,12 +46,12 @@
       }</span>
                 </td>
                 <td class="p-3">
-                  <a href="#" class="editTaskBtn hover:text-yellow-400 text-gray-700 mx-2">
+                  <span class="editTaskBtn hover:text-yellow-400 text-gray-700 mx-2">
                     <i class="material-icons-outlined text-base">edit</i>
-                  </a>
-                  <a href="#" class="deleteTaskBtn hover:text-red-500 text-gray-700 ml-2">
+                  </span>
+                  <span class="deleteTaskBtn hover:text-red-500 text-gray-700 ml-2">
                     <i class="material-icons-round text-base">delete_outline</i>
-                  </a>
+                  </span>
                 </td>
               </tr>`;
     });
@@ -59,4 +59,37 @@
     tasksList.innerHTML += HTML;
   };
   renderTodoList();
+
+  const addItemToDb = async () => {
+    return await axios
+      .post('/todo', {
+        name: addTaskNameInput.value,
+        done: addTaskDoneCheckbox.checked,
+      })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const resetToDefaultForm = () => {
+    addTaskDoneCheckbox.checked = false;
+    addTaskNameInput.value = '';
+  };
+
+  const taskItemDone = document.querySelectorAll('.taskItemDone');
+  const editTaskBtn = document.querySelectorAll('.editTaskBtn');
+  const deleteTaskBtn = document.querySelectorAll('.deleteTaskBtn');
+
+  addTaskBtn.addEventListener('click', async e => {
+    e.preventDefault();
+    if (addTaskNameInput.value === '') return;
+
+    tasks = await addItemToDb();
+
+    await renderTodoList();
+    resetToDefaultForm();
+  });
 })();
