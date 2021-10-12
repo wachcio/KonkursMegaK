93310cce4ca589e6cca673f2862e8e443f5d7560
wachcio <wachcio@poczta.fm>
2021-10-12 18:00:31 +0200

    change db folder name

--- helpers/variables.js
+++ helpers/variables.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 
-const DB_PATH = join(__dirname, '../DB/db.json');
+const DB_PATH = join(__dirname, '../db/db.json');
 let tasks;
 
 module.exports = { DB_PATH, tasks };
